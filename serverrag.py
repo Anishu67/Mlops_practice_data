17d5784f11e2cb949112b2f8464c36cb36eac363
shubham <shubhampalande41@gmail.com>
2025-12-28 23:56:53 +0530

    Make some changes in serverrag.py

--- serverrag.py
+++ serverrag.py
@@ -41,10 +41,10 @@ def get_current_user(credentials : HTTPAuthorizationCredentials = Depends(securi
     except JWTError:
         raise HTTPException(
             status_code= 401,
-            message = "Invalid token"
+            detail = "Invalid token"
         )
     
-app.post("/login")
+@app.post("/login")
 def login(data : LoginRequest):
     if data.username == "admin" and data.password == "admin123":
         token = create_access_token({"sub": data.username})
@@ -54,14 +54,74 @@ def login(data : LoginRequest):
         }
     raise HTTPException(
         status_code= 401,
-        message = "Invalid credentials"
+        detail = "Invalid credentials"
     )
-
+#rag initiation
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 raw_text = load_pdf_text("sample_policy.pdf")
 chunks = chunk_text(raw_text)
 
+embedding = embedding_model.encode(chunks)
+embedding = np.array(embedding).astype("float32")
 
+index = faiss.IndexFlatL2(embedding.shape[1])
+index.add(embedding)
+
+def ask_rag(question:str) ->str:
+    query_embedding = embedding_model.encode([question])
+    k = 3
+    distance, indices = index.search(np.array(query_embedding).astype("float32"), k = k)
+    retrieve_doc = [chunks[i] for i in indices[0]]
+    context = "\n".join(retrieve_doc)
+
+    prompt = f"""
+You are a strict question answering system.
+
+Rules:
+- Use ONLY the information present in the context.
+- Do NOT add extra explanations.
+- Do NOT invent policies, companies, or laws.
+- If the answer is present, respond in ONE short sentence.
+- If the answer is NOT present, respond exactly with: I don't know.
+
+context :
+{context}
+
+question :
+{question}
+
+"""
     
+    response = requests.post(
+         "http://localhost:11434/api/generate",
+        json = {
+            "model":"phi3:mini",
+            "prompt":prompt,
+            "stream" : False
+        }
 
+    )
+
+
+    data = response.json()
+    if "response" in data:
+        return data["response"]
+    else:
+        raise HTTPException(status_code=500, detail=data)
+
+@app.post("/chat")
+
+def chat(
+    data:ChatRequest,
+    user : str = Depends(get_current_user)
+):
+
+    answer = ask_rag(data.question)
+
+    return{
+        "user": user,
+        "question": data.question,
+        "answer": answer
+
+    }
