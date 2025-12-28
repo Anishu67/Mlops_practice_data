d741aaa2e2b35be7e0df20579a7e2d108b000b04
shubham <shubhampalande41@gmail.com>
2025-12-28 22:29:28 +0530

    2nd commit - We have succesfully wrote the fastapi code now next step is writing code for pdf loade and chunker

--- serverrag.py
+++ serverrag.py
@@ -12,5 +12,56 @@ import requests
 from pdf_loader import load_pdf_text
 from chunker import chunk_text
 
+app = FastAPI()
+security = HTTPBearer()
 
+#CONFIG
+SECRET_KEY = "your_secret_key"
+EXPIRE_IN_MINUTES = 30
+ALGORITHM = "HS256"
+
+class LoginRequest(BaseModel):
+    username : str
+    password : str  
+
+class ChatRequest(BaseModel):
+    question:str
+
+def create_access_token(data:dict):
+    payload = data.copy()
+    payload["exp"] = datetime.utcnow() + timedelta(minutes = EXPIRE_IN_MINUTES)
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(credentials : HTTPAuthorizationCredentials = Depends(security)):
+
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        raise HTTPException(
+            status_code= 401,
+            message = "Invalid token"
+        )
+    
+app.post("/login")
+def login(data : LoginRequest):
+    if data.username == "admin" and data.password == "admin123":
+        token = create_access_token({"sub": data.username})
+        return {
+            "access_token" : token,
+            "message" : "You have logged in Successfully"
+        }
+    raise HTTPException(
+        status_code= 401,
+        message = "Invalid credentials"
+    )
+
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+raw_text = load_pdf_text("sample_policy.pdf")
+chunks = chunk_text(raw_text)
+
+
+    
 
